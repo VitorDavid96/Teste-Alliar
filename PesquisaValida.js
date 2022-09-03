f4f74bcd0f92f58f4fd7515cd1e72ac593eb58cb
VitorDavid96 <112736819+VitorDavid96@users.noreply.github.com>
2022-09-02 21:59:06 -0300

    Add files via upload

---
+++ PesquisaValida.js
@@ -0,0 +1,30 @@
+
+
+const puppeteer = require('puppeteer'); 
+
+( async () => {
+
+   const browser = await puppeteer.launch({
+      handless:false
+   });
+   
+   const page = await browser.newPage();
+   await page.viewport({
+      width:1920,
+      height:1080
+   })
+
+   await page.goto ('https://www.olx.com.br/');
+   await page.waitForSelector ('input[id="searchtext-input"]');
+   await page.type('input[id="searchtext-input"]', 'tenis',{delay:100});
+   
+   //await page.waitForSelector ('[class="search-button-submit"]');
+   await page.click('[class="search-button-submit"]')
+
+
+   
+
+   await page.screenshot ({path:'teste_alliar_valido.png'});
+   await browser.close();
+})();
+
